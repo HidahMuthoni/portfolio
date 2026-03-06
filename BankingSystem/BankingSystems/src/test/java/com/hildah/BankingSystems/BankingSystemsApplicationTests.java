15349f5d97cbbfff6f64af59931a3409f262a89d
Susan Ngendo <ngendosusan101@gmail.com>
2026-03-06 16:44:17 +0300

    Add BankingSystem project as a normal folder

---
+++ BankingSystem/BankingSystems/src/test/java/com/hildah/BankingSystems/BankingSystemsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hildah.BankingSystems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingSystemsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ BankingSystem/src/main/java/com/hildah/BankingSystem/controller/WeatherController.java
@@ -0,0 +1,27 @@
+package com.hildah.bankingsystem.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class WeatherController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+
+    @PostMapping("/checkBalance")
+    public String checkBalance(@RequestParam("account") String account, Model model) {
+        // Fake logic for demo
+        if (account.equals("12345")) {
+            model.addAttribute("balance", 1000);
+        } else {
+            model.addAttribute("error", "Account not found.");
+        }
+        return "index";
+    }
+}
